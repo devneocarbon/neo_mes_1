58ad0bf8580b58f6037fb03434523c05a34f0204
nov5lll <97242918+nov5lll@users.noreply.github.com>
2022-09-04 13:52:54 +0200

    primeiro formulário

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -4,12 +4,17 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-
+import { Button, FormControl, FormLabel, Radio, RadioGroup } from '@mui/material';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 export default function AddressForm() {
+      const [value, setValue] = React.useState(dayjs());
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Shipping address
+        Informações do projeto
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
@@ -17,7 +22,7 @@ export default function AddressForm() {
             required
             id="firstName"
             name="firstName"
-            label="First name"
+            label="Nome do proprietário"
             fullWidth
             autoComplete="given-name"
             variant="standard"
@@ -28,7 +33,7 @@ export default function AddressForm() {
             required
             id="lastName"
             name="lastName"
-            label="Last name"
+            label="Código do projeto"
             fullWidth
             autoComplete="family-name"
             variant="standard"
@@ -39,38 +44,45 @@ export default function AddressForm() {
             required
             id="address1"
             name="address1"
-            label="Address line 1"
+            label="Nome do projeto"
             fullWidth
             autoComplete="shipping address-line1"
             variant="standard"
           />
         </Grid>
         <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-            variant="standard"
-          />
+            <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Tipo de crédito</FormLabel>
+            <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+            >
+                <FormControlLabel value="female" control={<Radio />} label="Unidade de carbono florestal - UCF" />
+                <FormControlLabel value="male" control={<Radio />} label="Unidade de crédito de recursos hidricos -
+            UCRH" />
+                <FormControlLabel value="other" control={<Radio />} label="Unidade de crédito de biodiversidade -
+            UCBIO" />
+            </RadioGroup>
+            </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-          />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+       <MobileDatePicker
+          label="Data de Registro"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+        </LocalizationProvider>
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
             id="state"
             name="state"
-            label="State/Province/Region"
+            label="Município/Estado"
             fullWidth
             variant="standard"
           />
@@ -80,7 +92,7 @@ export default function AddressForm() {
             required
             id="zip"
             name="zip"
-            label="Zip / Postal code"
+            label="Latitude do projeto (graus decimais):"
             fullWidth
             autoComplete="shipping postal-code"
             variant="standard"
@@ -91,18 +103,49 @@ export default function AddressForm() {
             required
             id="country"
             name="country"
-            label="Country"
+            label="Longitude do projeto (graus decimais):"
             fullWidth
             autoComplete="shipping country"
             variant="standard"
           />
         </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
+
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            id="country"
+            name="country"
+            label="Área do projeto"
+            fullWidth
+            autoComplete="shipping country"
+            variant="standard"
           />
         </Grid>
+
+        <Grid item xs={12} sm={6}>
+            <Typography gutterBottom>
+                Área do projeto em .KML
+            </Typography>
+            <Button variant="contained" component="label">
+            Upload
+            <input hidden accept="image/*" multiple type="file" />
+            </Button>
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+             multiline
+          maxRows={4}
+            id="country"
+            name="country"
+            label="Descrição do projeto"
+            fullWidth
+            autoComplete="shipping country"
+            variant="standard"
+          />
+        </Grid>
+
       </Grid>
     </React.Fragment>
   );
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -16,20 +16,9 @@ import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import Review from './Review';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
-const steps = ['Shipping address', 'Payment details', 'Review your order'];
+
+const steps = ['Informações do projeto', 'Informações do cliente', 'Review seu projeto'];
 
 function getStepContent(step) {
   switch (step) {
@@ -69,16 +58,12 @@ export default function Checkout() {
           borderBottom: (t) => `1px solid ${t.palette.divider}`,
         }}
       >
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Company name
-          </Typography>
-        </Toolbar>
+
       </AppBar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center">
-            Checkout
+            Cadastro
           </Typography>
           <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
             {steps.map((label) => (
@@ -90,14 +75,10 @@ export default function Checkout() {
           <React.Fragment>
             {activeStep === steps.length ? (
               <React.Fragment>
-                <Typography variant="h5" gutterBottom>
-                  Thank you for your order.
+                <Typography margin="center" variant="h5" gutterBottom>
+                  Projeto cadastrado, atualize a página!
                 </Typography>
-                <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order
-                  confirmation, and will send you an update when your order has
-                  shipped.
-                </Typography>
+
               </React.Fragment>
             ) : (
               <React.Fragment>
@@ -105,7 +86,7 @@ export default function Checkout() {
                 <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
                   {activeStep !== 0 && (
                     <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                      Back
+                      Voltar
                     </Button>
                   )}
 
@@ -114,14 +95,13 @@ export default function Checkout() {
                     onClick={handleNext}
                     sx={{ mt: 3, ml: 1 }}
                   >
-                    {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                    {activeStep === steps.length - 1 ? 'Enviar' : 'Próximo'}
                   </Button>
                 </Box>
               </React.Fragment>
             )}
           </React.Fragment>
         </Paper>
-        <Copyright />
       </Container>
     </ThemeProvider>
   );
