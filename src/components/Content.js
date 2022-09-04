6ecb05332290363b56871a6646615f1a1a17f3bf
nov5lll <97242918+nov5lll@users.noreply.github.com>
2022-09-04 12:50:30 +0200

    test

---
+++ src/components/Content.js
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import { Modal, Box } from '@mui/material';
+import Checkout from './Checkout';
+
+
+
+export default function Content() {
+
+  const style = {
+    position:'absolute',
+
+    overflow:'scroll',
+    height:'100%',
+    display:'block'
+  };
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+  return (
+    <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
+      >
+        <Toolbar>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <SearchIcon color="inherit" sx={{ display: 'block' }} />
+            </Grid>
+            <Grid item xs>
+              <TextField
+                fullWidth
+                placeholder="Busque pelo ID do projeto"
+                InputProps={{
+                  disableUnderline: true,
+                  sx: { fontSize: 'default' },
+                }}
+                variant="standard"
+              />
+            </Grid>
+            <Grid item>
+              <Button onClick={handleOpen} variant="contained" sx={{ mr: 1 }}>
+
+                Adicionar novo projeto
+                <Modal
+                  sx={style}
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Checkout/> 
+                </Modal>
+              </Button>
+              <Tooltip title="Reload">
+                <IconButton>
+                  <RefreshIcon color="inherit" sx={{ display: 'block' }} />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center">
+        Ainda não há projetos para mostrar
+      </Typography>
+    </Paper>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+function Header(props) {
+  const { onDrawerToggle } = props;
+
+  return (
+    <React.Fragment>
+      <AppBar color="primary" position="sticky" elevation={0}>
+        <Toolbar>
+          <Grid container spacing={1} alignItems="center">
+            <Grid sx={{ display: { sm: 'none', xs: 'block' } }} item>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={onDrawerToggle}
+                edge="start"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <AppBar
+        component="div"
+        position="static"
+        elevation={0}
+        sx={{ zIndex: 0 }}
+      >
+        <Toolbar>
+          <Grid container alignItems="center" spacing={1}>
+            <Grid item xs>
+              <Typography color="#fff" variant="h5" component="h1">
+                Cadastro de Projetos
+              </Typography>
+            </Grid>
+
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <AppBar component="div" position="static" elevation={0} sx={{ zIndex: 0 }}>
+        <Tabs value={0} textColor="inherit">
+          <Tab label="Projetos" />
+
+        </Tabs>
+      </AppBar>
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  onDrawerToggle: PropTypes.func.isRequired,
+};
+
+export default Header;
